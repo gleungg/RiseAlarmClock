087830a5c35430e451af416220c820e6811d4ac6
gleungg <gordon.leung@alumni.ubc.ca>
2019-10-13 07:30:57 -0700

    'hel'

---
+++ app/src/main/java/com/example/clock/AlarmRings.java
@@ -0,0 +1,14 @@
+package com.example.clock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlarmRings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_rings);
+    }
+}
---
+++ app/src/main/java/com/example/clock/NewAlarmForm.java
@@ -0,0 +1,14 @@
+package com.example.clock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewAlarmForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_alarm_form);
+    }
+}
